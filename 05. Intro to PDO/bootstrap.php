669839824c2a2931002dbf5ee4dff7466305023d
tomasbaublys <tomas@forumcinemashe.lt>
2017-12-16 16:25:45 +0200

    Create db config.php file

--- 05. Intro to PDO/bootstrap.php
+++ 05. Intro to PDO/bootstrap.php
@@ -1,15 +1,16 @@
 <?php 
 
+$config = require 'config.php';
 require 'database/Connection.php';
 require 'database/QueryBuilder.php';
 
-/* class Connection static method make() returns PDO, we saving it to $pdo variable */
-$pdo = Connection::make();
+/* class Connection static method make() returns PDO, we saving it to $pdo variable, it gets db configuration parameter from config.php database array*/
+$pdo = Connection::make($config['database']);
 
 /* we initiate new QueryBuilder and giving $pdo variable */
 $query = new QueryBuilder($pdo);
 
 /* we also could use short way:
-$query = new QueryBuilder(Connection::make()); */
+$query = new QueryBuilder(Connection::make($config['database'])); */
 
 
--- 05. Intro to PDO/database/Connection.php
+++ 05. Intro to PDO/database/Connection.php
@@ -3,10 +3,20 @@
 // Connecting to PDO
 class Connection
 {
-	public static function make()
+	// takes all database configuration file from config.php
+	public static function make($config)
 	{
 		try {
-			return new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'root', '');	
+			/* Incase this db configuration data:
+			return new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'root', '');
+			we could use this one below:*/
+			return new PDO(
+				$config['connection'].';dbname='.$config['name'],
+				$config['username'],
+				$config['password'],
+				$config['options']
+
+			);		
 		} catch (PDOException $e) {
 			die($e->getMessage());
 		}
