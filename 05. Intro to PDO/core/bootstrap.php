998371b10e310c9486b74d3e74958d33191989ab
tomasbaublys <tomas@forumcinemashe.lt>
2017-12-22 16:37:09 +0200

    Install Composer for dependency managment and autoloading

--- 05. Intro to PDO/core/bootstrap.php
+++ 05. Intro to PDO/core/bootstrap.php
@@ -1,11 +1,6 @@
 <?php
 $app = [];
 $app['config'] = require 'config.php';
-require 'core/Router.php';
-require 'core/Request.php';
-require 'core/database/Connection.php';
-require 'core/database/QueryBuilder.php';
-
 
 /* class Connection static method make() returns PDO, we saving it to $pdo variable, it gets db configuration parameter from config.php database array
 $pdo = Connection::make($config['database']); */
