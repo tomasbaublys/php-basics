940264d3ba756071543f36df34cb2411ed94afd8
tomasbaublys <tomas@forumcinemashe.lt>
2018-01-19 11:35:10 +0200

    Finish 08. Intro to PDO project

--- 08. Intro to PDO/core/bootstrap.php
+++ 08. Intro to PDO/core/bootstrap.php
@@ -35,3 +35,5 @@ function redirect($path)
 
 
 
+
+
--- 08. Intro to PDO/index.php
+++ 08. Intro to PDO/index.php
@@ -1,5 +1,6 @@
 <?php 
 
+// in PHP 7 we could do like this: use App\Core\{Router, Request};
 use App\Core\Router;
 use App\Core\Request;
 
@@ -15,3 +16,4 @@ Router::load('app/routes.php')
 3.	Class Router direct() method takes $uri from Request uri() method and $requestType from method() method and returns callAction method by exploding controller in 2 parts, for example $controller->UserController and $action->index;
 4.	Class Router callAction() method assign and returns relevant action for controller, which we describe in given ...Controller.php file;
  */
+
